285e1b9f173d5e5646aacc11a487fe6ab4f93a0d
nadhirasya <nadhirasya@student.ub.ac.id>
2026-05-18 18:16:56 +0700

    Menambahkan fitur categories

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CampaignController;
+use App\Http\Controllers\CategoryController;
 
 // Endpoint Modul Fondasi (Auth)
 Route::prefix('auth')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
 });
+
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::post('/categories', [CategoryController::class, 'store']);
 
 // Endpoint Modul Campaign
 // Publik bisa melihat daftar campaign
